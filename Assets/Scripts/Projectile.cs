e3c30ca9eeb98189fc7e47071d846a7527750c63
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-05 16:43:57 +0100

    Create projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 15;
+    [SerializeField]
+    private float lifetime = 5;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Destroy(gameObject, lifetime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + transform.forward * speed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
