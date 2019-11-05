39eeed23d9ccc0307f4c74a229e272fa54d6ed5f
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-05 16:46:58 +0100

    Create player shooting script

---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField]
+    private Gun startingGun;
+    [SerializeField]
+    private Transform gunHold;
+
+    private Gun equippedGun;
+
+    private void Start()
+    {
+        EquipGun(startingGun);
+    }
+
+    public void EquipGun(Gun gunToEquip)
+    {
+        if (equippedGun != null)
+        {
+            Destroy(equippedGun);
+        }
+        equippedGun = Instantiate(gunToEquip, gunHold.position, gunHold.rotation, gunHold);
+    }
+
+    public void TryShooting()
+    {
+        equippedGun.TryShooting();
+    }
+}
