3d9a5fd09efaea565a4bfaa8112271bfcabf81ad
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-05 16:45:25 +0100

    Create Gun script

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField]
+    private Transform shootingPoint;
+    [SerializeField]
+    private Projectile projectilePrefab;
+    [SerializeField]
+    private float fireRate;
+
+    private float timeUntilNextShot;
+
+    private void Update()
+    {
+        timeUntilNextShot -= Time.deltaTime;
+    }
+
+    public void TryShooting()
+    {
+        if (timeUntilNextShot >= 0)
+        {
+            return;
+        }
+        timeUntilNextShot = 1 / fireRate;
+        Instantiate(projectilePrefab, shootingPoint.position, shootingPoint.rotation);
+    }
+}
