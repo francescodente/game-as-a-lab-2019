03ffec946aae222d3454bac5a4b044e5b69161c6
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-07 16:15:39 +0100

    Create EnemyController script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float pathUpdateRate = 4;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        StartCoroutine(UpdateDestination());
+    }
+
+    private IEnumerator UpdateDestination()
+    {
+        WaitForSeconds wait = new WaitForSeconds(1 / pathUpdateRate);
+        while (target != null)
+        {
+            agent.SetDestination(target.position);
+            yield return wait;
+        }
+    }
+}
