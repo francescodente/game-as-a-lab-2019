a081c8ddb82956b8be7d85a32a3c03fed758ebdf
Francesco Dente <dente.francesco1996@gmail.com>
2019-11-16 16:20:52 +0100

    Create character health

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField]
+    private int startingHealth = 100;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
